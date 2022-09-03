acca285ab5fd11104cc87e9f8942422754e05e5a
Neustod <vane90989@gmail.com>
2022-09-03 15:09:28 +0300

    Project

---
+++ Project1/IntArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    class IntArray
+    {
+        public IntArray(uint size) { Index = new int[size]; this.size = size; }
+
+        private uint size;
+
+        public int[] Index { get; set; }
+        public uint Size { get => size; }
+
+        static public IntArray Create(uint size) => new IntArray(size);
+        public void FillRand(int start, int end)
+        {
+            Random temp = new Random();
+            for (int i = 0; i < Size; i++) Index[i] = temp.Next() % (end - start + 1) + start;
+        }
+        public int MaxValue()
+        {
+            int maxv = Index[0];
+            foreach(int item in Index) { if (maxv < item) maxv = item; }
+            return maxv;
+        }
+        public int MinValue()
+        {
+            int minv = Index[0];
+            foreach (int item in Index) { if (minv > item) minv = item; }
+            return minv;
+        }
+        public void Print()
+        {
+            foreach (int item in Index) { Console.Write($"{item} "); }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project1 
+{    
+    class Program 
+    {      
+        static void Main() 
+        {
+            IntArray Arr = IntArray.Create(size: 10);
+            Arr.FillRand(-100, 100);
+
+            Arr.Print();
+            Console.WriteLine($"Max value = {Arr.MaxValue()}\n" +
+                $"Min value = {Arr.MinValue()}");
+
+            Console.ReadLine();
+        }
+    }
+}
